fe308a7610cec5a5951262a4a5795c92acce9b2d
DUTRA, LUIS - Dell Team <LUIS_DUTRA@Dell.com>
2022-11-02 15:54:17 -0300

    Order by LastName

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,23 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      List<Person> people = ListManager.LoadSampleData();
+        static void Main(string[] args)
+        {
+            List<Person> people = ListManager.LoadSampleData();
 
-      foreach (var person in people)
-      {
-        Console.WriteLine($"{ person.FirstName } { person.LastName } ({ person.Birthday.ToShortDateString() }): Experience { person.YearsExperience }");
-      }
+            people = people.OrderBy(x => x.LastName).ToList();
 
-      Console.ReadLine();
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{ person.FirstName } { person.LastName } ({ person.Birthday.ToShortDateString() }): Experience { person.YearsExperience }");
+            }
+
+            Console.ReadLine();
+        }
     }
-  }
 }
